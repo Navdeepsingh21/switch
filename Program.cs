3e2c88f45ec18f402a52e5c31407e1cd4dca7eeb
Admin <Admin@DESKTOP-POQE336>
2019-05-21 11:05:57 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @switch
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter any char");
+            char ch = char.Parse(Console.ReadLine());
+            string str = ch.ToString().ToLower();
+            switch (str)
+            {
+                case "a":
+                    Console.WriteLine(str + "is a vowel");
+                    break;
+                case "e":
+                    Console.WriteLine(str + "is a vowel");
+                    break;
+                case "i":
+                    Console.WriteLine(str + "is a vowel");
+                    break;
+                case "o":
+                    Console.WriteLine(str + "is a vowel");
+                    break;
+                case "u":
+                    Console.WriteLine(str + "is a vowel");
+                    break;
+
+            }
+        }
+    }
+}
