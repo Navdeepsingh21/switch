27b7884826b31ec571dc09fea949229e94ded099
Admin <Admin@DESKTOP-POQE336>
2019-05-21 11:08:37 +1200

    name change

--- Program.cs
+++ Program.cs
@@ -4,34 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace @vowel coding
+namespace @switch
 {
     static class Program
-{
-    static void Main(string[] args)
     {
-        Console.WriteLine("enter any char");
-        char ch = char.Parse(Console.ReadLine());
-        string str = ch.ToString().ToLower();
-        switch (str)
+        static void Main(string[] args)
         {
-            case "a":
-                Console.WriteLine(str + "is a vowel");
-                break;
-            case "e":
-                Console.WriteLine(str + "is a vowel");
-                break;
-            case "i":
-                Console.WriteLine(str + "is a vowel");
-                break;
-            case "o":
-                Console.WriteLine(str + "is a vowel");
-                break;
-            case "u":
-                Console.WriteLine(str + "is a vowel");
-                break;
+            Console.WriteLine("enter any char");
+            char ch = char.Parse(Console.ReadLine());
+            string str = ch.ToString().ToLower();
+            switch (str)
+            {
+                case "a":
+                    Console.WriteLine(str + "is a vowel");
+                    break;
+                case "e":
+                    Console.WriteLine(str + "is a vowel");
+                    break;
+                case "i":
+                    Console.WriteLine(str + "is a vowel");
+                    break;
+                case "o":
+                    Console.WriteLine(str + "is a vowel");
+                    break;
+                case "u":
+                    Console.WriteLine(str + "is a vowel");
+                    break;
 
+            }
         }
     }
-}
 }
